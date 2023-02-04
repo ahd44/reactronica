cb5fcbe71e93ad9e1a843d371ba475cfe914a71a
ahd44 <75226091+ahd44@users.noreply.github.com>
2023-02-04 10:03:55 -0600

    Removing commented out code for linter

--- src/components/Track.tsx
+++ src/components/Track.tsx
@@ -123,28 +123,11 @@ const TrackConsumer: React.FC<TrackConsumerProps> = ({
 
   useEffect(() => {
     if (sequencer.current) {
-      /*if (prevSequencerSteps?.length === sequencerSteps.length) {
-        // When steps length is the same, update steps in a more efficient way
-        sequencerSteps.forEach((step, i) => {
-          const isEqual = equal(
-            sequencerSteps[i].notes,
-            prevSequencerSteps && prevSequencerSteps[i]
-              ? prevSequencerSteps[i].notes
-              : [],
-          );
-
-          if (!isEqual) {
-            sequencer.current?.remove(i);
-            sequencer.current?.add(i, step);
-          }
-        });
-      } else {*/
-        // When new steps are less or more then prev, remove all and add new steps
-        sequencer.current.removeAll();
-        sequencerSteps.forEach((step, i) => {
-          sequencer.current.add(i, step);
-        });
-      //}
+      // When new steps are less or more then prev, remove all and add new steps
+      sequencer.current.removeAll();
+      sequencerSteps.forEach((step, i) => {
+        sequencer.current.add(i, step);
+      });
     }
     /* eslint-disable-next-line */
   }, [JSON.stringify(sequencerSteps)]);
